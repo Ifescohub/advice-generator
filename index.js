e61fd719b50dd72b43da00fb74cb208188251066
Udeh Ifeanyi Monday <ifescoguy@gmail.com>
2022-09-26 17:44:56 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+document.getElementById("dice-icon").addEventListener("click", ()=>{
+    fetch("https://api.adviceslip.com/advice")
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("advice-id").textContent = data.slip.id;
+            document.getElementById("advice").textContent = data.slip.advice;
+        });
+});
